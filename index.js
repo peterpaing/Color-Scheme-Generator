502888fb317cbdabed3df781f5e731248b85b12e
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-18 21:19:46 +0630

    render js and connect with API GET method

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('generate-btn').addEventListener('click', function(e){
+        e.preventDefault()
+        const color =document.getElementById('color-input').value
+        const scheme= document.getElementById('color-scheme').value 
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color.slice(1)}&mode=${scheme}&count=5`)
+    .then(res => res.json())
+    .then(data => {
+        let colors = data.colors.map(color=>{
+        return `<div class="color-container">
+        <img src="${color.image.bare}">
+        <p>${color.hex.value}</p>
+        </div>`
+        })
+     document.getElementById('colorImage').innerHTML = colors.join('')
+    })
+})
